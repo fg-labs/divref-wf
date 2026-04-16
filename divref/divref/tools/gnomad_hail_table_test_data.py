b7cde5574f80dd028b9cb95b8721a6fe23ddf037
Alison Meynert <alison@fulcrumgenomics.com>
2026-04-16 11:45:12 -0700

    feat: update fetching test data

--- divref/divref/tools/gnomad_hail_table_test_data.py
+++ divref/divref/tools/gnomad_hail_table_test_data.py
@@ -15,22 +15,25 @@ logger = logging.getLogger(__name__)
 def gnomad_hail_table_test_data(
     *,
     in_gnomad_hgdp_variant_annotation_table: HailPath = defaults.GNOMAD_HGDP_1KG_VARIANT_ANNOTATION_HAIL_TABLE,  # noqa: E501
+    in_gnomad_hgdp_sample_metadata: HailPath = defaults.GNOMAD_HGDP_1KG_SAMPLE_METADATA_HAIL_TABLE,  # noqa: E501
     out_variant_annotation_table: HailPath,
+    out_sample_metadata: HailPath,
     locus: str = "chr1:100001-200000",
     sample_proportion: float = 0.01,
     seed: int = 42,
     gcs_credentials_path: Path = Path("~/.config/gcloud/application_default_credentials.json"),
 ) -> None:
     """
-    Extracts a subset of the gnomAD HGDP/1KG variant annotations for testing.
+    Extract subsets of gnomAD HGDP/1KG variant annotations and sample metadata for testing.
 
     Args:
         in_gnomad_hgdp_variant_annotation_table: Path to the gnomAD HGDP/1KG variant annotation
             Hail table.
         in_gnomad_hgdp_sample_metadata: Path to the gnomAD HGDP/1KG sample metadata Hail table.
         out_variant_annotation_table: Output path for the subset variant annotation Hail table.
-        out_sample_metadata: Output path for the subset sample metadata Hail table.
-        locus: Sampling locus.
+        out_sample_metadata: Output path for the sample metadata Hail table, stripped to key and
+            gnomad_population_inference only.
+        locus: Sampling locus for variant filtering.
         sample_proportion: Proportion of variants to sample.
         seed: Random sampling seed.
         gcs_credentials_path: Path to GCS default credentials JSON file.
@@ -42,6 +45,12 @@ def gnomad_hail_table_test_data(
 
     logger.info(f"Filtering to {locus} and sampling {sample_proportion} of variants.")
     va_subset = hl.filter_intervals(va, roi).sample(sample_proportion, seed)
-    logger.info(f"{va_subset.count()} variants found.")
 
+    logger.info(f"Writing {va_subset.count()} variants to {out_variant_annotation_table}.")
     va_subset.write(out_variant_annotation_table, overwrite=True)
+
+    sa = hl.read_table(in_gnomad_hgdp_sample_metadata)
+    sa = sa.select("gnomad_population_inference").select_globals()
+
+    logger.info(f"Writing {sa.count()} samples to {out_sample_metadata}.")
+    sa.naive_coalesce(1).write(out_sample_metadata, overwrite=True)
