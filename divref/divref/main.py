ebfe5269c26fe772d613e1d905215f2e56535de2
Alison Meynert <alison@fulcrumgenomics.com>
2026-04-16 20:24:16 -0700

    feat: add tool to main

--- divref/divref/main.py
+++ divref/divref/main.py
@@ -13,6 +13,7 @@ from divref.tools.create_gnomad_sites_vcf import create_gnomad_sites_vcf
 from divref.tools.extract_gnomad_afs import extract_gnomad_afs
 from divref.tools.extract_sample_metadata import extract_sample_metadata
 from divref.tools.gnomad_hail_table_test_data import gnomad_hail_table_test_data
+from divref.tools.remap_divref import remap_divref
 
 _tools: List[Callable[..., None]] = [
     compute_haplotypes,
@@ -23,6 +24,7 @@ _tools: List[Callable[..., None]] = [
     extract_gnomad_afs,
     extract_sample_metadata,
     gnomad_hail_table_test_data,
+    remap_divref,
 ]
 
 
